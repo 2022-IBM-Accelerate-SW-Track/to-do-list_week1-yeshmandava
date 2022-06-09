fb8497a4a017aa1ab2fc53ea4d961ef96d701e43
yeshmandava <yeshwanthmandava10@gmail.com>
2022-06-08 21:31:57 -0400

    Inital chnages to about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import "./About.css";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          // Image goes here
+          alt="Profile Pic"
+          ></img>
       </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Yeshwanth Mandava</div>
+        <div className="brief_description">
+          What's up my dawg
+        </div>
+      </div>
+    </div>
+  </div>
     )
   }
 }
